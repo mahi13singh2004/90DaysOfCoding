441d529257624ef67a64073dd802ba48cb299c6e
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-01-29 21:51:48 +0530

    Update reversearray.cpp

--- CODE/DAY1/reversearray.cpp
+++ CODE/DAY1/reversearray.cpp
@@ -1,31 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MOD=1e9+7;
+const int MOD = 1e9 + 7;
 const long long INF = LLONG_MAX / 2;
     
-void solve(){
+void solve() {
     int N;
-    cin>>N;
+    cin >> N;
 
     int array[N];
-    for(int i=0; i<N; i--){
-      cin>>array[i];
-      
-    cout<<"Original Array is:"<<endl;
-
-    for(int i=0; i<N; i++){
-      cout<<array[i]<<" ";
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
     }
 
-    cout<<"Reversed Array is:"<<endl;
+    cout << "Original Array is:" << endl;
 
-    for(int i=N-1; i>=0; i++){
-        cout<<array[i]<<" ";
+    for (int i = 0; i < N; i++) {
+        cout << array[i] << " ";
     }
+
+    cout << "\nReversed Array is:" << endl;
+
+    for (int i = N - 1; i >= 0; i--) {
+        cout << array[i] << " ";
+    }
+    cout << endl; 
 }
-}
-    
-signed main(){
-        solve();
-return 0;
+
+int main() {
+    solve();
+    return 0;
 }
