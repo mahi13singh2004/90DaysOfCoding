e2ee87bffbd476eb164f82d0c63c039b59f3f20e
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-01-29 21:48:15 +0530

    Update linearsearch.cpp

--- CODE/DAY1/linearsearch.cpp
+++ CODE/DAY1/linearsearch.cpp
@@ -1,36 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int MOD=1e9+7;
+const int MOD = 1e9 + 7;
 const long long INF = LLONG_MAX / 2;
 
-bool find_linear(int array[], int N, int key){
-    bool flag=false;
-    for(int i=0; i<N; i++){
-        if(array[i]==key){
-            flag=true;
+bool find_linear(int array[], int N, int key) {
+    bool flag = false;
+    for (int i = 0; i < N; i++) {
+        if (array[i] == key) {
+            flag = true; // set flag to true when key is found
+            cout << "Found" << endl;
             break;
         }
     }
     return flag;
 }
-    
-void solve(){
+
+void solve() {
     int N;
-    cin>>N;
+    cin >> N;
 
     int array[N];
-    for(int i=0; i<N; i++){
-      cin>>array[i];
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
 
     int key;
-    cin>>key;
+    cin >> key;
 
-    bool answer=find_linear(array,N,key);
-    cout<<answer<<endl;
-    }
+    bool answer = find_linear(array, N, key);
+    cout << answer << endl;
 }
-    
-signed main(){
-        solve();
-return 0;
+
+int main() {
+    solve();
+    return 0;
 }
