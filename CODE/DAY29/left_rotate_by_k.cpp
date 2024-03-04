a3fe1bd2b1d7de4103ccf370d7c072f296098b14
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-03-04 14:23:24 +0530

    Create left_rotate_by_k.cpp

---
+++ CODE/DAY29/left_rotate_by_k.cpp
@@ -0,0 +1,38 @@
+vector<int> rotateArray(vector<int>array, int K) {
+    int N=array.size();
+    K=K%N;
+    vector<int>empty(K);
+    for(int i=0; i<K; i++){
+        empty[i]=array[i];
+    }
+
+    for(int i=K; i<N; i++){
+        array[i-K]=array[i];
+    }
+
+    for(int i=N-K; i<N; i++){
+        array[i]=empty[i-(N-K)];
+    }
+
+    return array;
+}
+
+//OPTIMAL
+vector<int> rotateArray(vector<int>array, int k) {
+    int N=array.size();
+    reverse(array.begin(),array.begin()+k);
+    reverse(array.begin()+k, array.end());
+    reverse(array.begin(),array.end());
+    return array;
+}
+
+//Better to use reverse function instead of inbuilt:
+//while(start<=end){
+//int temp=array[start];
+//array[start]=array[end];
+//array[end]=temp;
+//start++;
+//end--;
+//}
+//return array
+//Here start and end are iterators pointing to index
